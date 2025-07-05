17b98d92f38b544d1f00ffee041ae6daf1e84436
Sergio Galera <sgale.developer@gmail.com>
2025-07-06 01:10:48 +0200

    Current value being updated

--- chart-race/core/src/commonMain/kotlin/com/sgale/chart_core/barchart/BarChartEntry.kt
+++ chart-race/core/src/commonMain/kotlin/com/sgale/chart_core/barchart/BarChartEntry.kt
@@ -1,3 +1,19 @@
+/*
+ * Designed and developed by 2025 sgaleraalq (Sergio Galera)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sgale.chart_core.barchart
 
 import com.sgale.chart_core.AbstractChartEntry
@@ -8,7 +24,7 @@ class BarChartEntry <T: Number> (
 ) : AbstractChartEntry() {
 
     override fun updateCurrentValue(time: Float) {
-        entryModel.currentValue.plus(1)
+        entryModel.currentValue += time
         println("Entry label: ${entryModel.label}, current value: ${entryModel.currentValue}")
     }
 }
