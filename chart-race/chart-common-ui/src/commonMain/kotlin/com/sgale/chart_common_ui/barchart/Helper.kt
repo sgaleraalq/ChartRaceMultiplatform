2a967d8791b341a89282765f3a273c319bfceeae
Sergio Galera <sgale.developer@gmail.com>
2025-07-05 11:46:07 +0200

    BarChart Helper created

---
+++ chart-race/chart-common-ui/src/commonMain/kotlin/com/sgale/chart_common_ui/barchart/Helper.kt
@@ -0,0 +1,13 @@
+package com.sgale.chart_common_ui.barchart
+
+import androidx.compose.ui.graphics.drawscope.DrawScope
+
+/**
+ * Canvas draw scope to draw a bar at the specified position.
+ */
+fun DrawScope.drawBar(
+    x: Float,
+    y: Float
+) {
+    // TODO
+}
